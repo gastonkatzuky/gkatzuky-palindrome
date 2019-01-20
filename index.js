d03eb2d3e71f3605d55258d08d02012561b64ec3
Gaston Katzuky <iatzkypedro@gmail.com>
2019-01-20 10:42:07 -0300

    BugFix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function Phrase(content) {
 
   // Returns the letters in the content.
   this.letters = function letters() {
-    return (this.content.match(/[a-z]/gi) || []).join(""));
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true for a palindrome, false otherwise.
